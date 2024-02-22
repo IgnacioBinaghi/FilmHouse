9a41ad564c9fb1f9f8f5ceba46662d51bd39bf6c
IgnacioBinaghi <binaghiignacio@gmail.com>
2024-02-22 11:36:29 -0500

    fixes to package.json

--- index.js
+++ index.js
@@ -173,4 +173,7 @@ app.get('/user/:username', async (req, res) => {
 });
 
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
